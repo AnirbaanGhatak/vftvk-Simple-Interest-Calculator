2b84c93583f3f6e0454c80f2095f7411752cff8d
Anirbaan Ghatak <anirbaanghatak1.ag@gmail.com>
2022-05-15 11:01:24 +0530

    colors done"

--- script.js
+++ script.js
@@ -8,18 +8,24 @@ function compute() {
     r = document.getElementById('rate').value;
     t = document.getElementById('years').value;
 
-    console.log(typeof(p) + "  " + typeof(r));
-
-    let yr = new Date().getFullYear().toString();
-    yr = (parseInt(yr) + parseInt(t)).toString();
-
-    ans = ((p*r*t)/100);
+    if (p == " " || p == "0" || parseInt(p) < 0) {
+        alert("Please Enter a Positive number");
+    } else {
 
 
-    // console.log(yr);
 
-    document.getElementById('ci').innerHTML = `If you deposit ${p},<br/> at an interest rate of ${r}%.<br/> You will recieve an amount of ${ans},<br/> in the year ${yr}`;
+        console.log(typeof (p) + "  " + typeof (r));
 
+        let yr = new Date().getFullYear().toString();
+        yr = (parseInt(yr) + parseInt(t)).toString();
+
+        ans = ((p * r * t) / 100);
+
+
+        // console.log(yr);
+
+        document.getElementById('ci').innerHTML = `If you deposit <mark>${p}</mark>,<br/> at an interest rate of <mark>${r}%</mark>.<br/> You will recieve an amount of <mark>${ans}</mark>,<br/> in the year <mark>${yr}</mark>`;
+    }
 }
 
 
@@ -33,12 +39,12 @@ const min = slider.min;
 const max = slider.max;
 const value = slider.value;
 
-slider.style.background = `linear-gradient(to right, blue 0%, blue ${(value - min) / (max - min) * 100}%, #DEE2E6 ${(value - min) / (max - min) * 100}%, #DEE2E6 100%)`;
+slider.style.background = `linear-gradient(to right, #1073F5 0%, #1073F5 ${(value - min) / (max - min) * 100}%, #DEE2E6 ${(value - min) / (max - min) * 100}%, #DEE2E6 100%)`;
 
 slider.oninput = function () {
-    
-    
-    slider.style.background = `linear-gradient(to right, blue 0%, blue ${(this.value - this.min) / (this.max - this.min) * 100}%, #DEE2E6 ${(this.value - this.min) / (this.max - this.min) * 100}%, #DEE2E6 100%)`
+
+
+    slider.style.background = `linear-gradient(to right, #1073F5 0%, #1073F5 ${(this.value - this.min) / (this.max - this.min) * 100}%, #DEE2E6 ${(this.value - this.min) / (this.max - this.min) * 100}%, #DEE2E6 100%)`
 };
 
 
